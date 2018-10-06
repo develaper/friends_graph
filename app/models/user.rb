e7252060b9f46613b5f352aeb8a728c9ac14a87d
develaper <lachguer.brahim@gmail.com>
2018-10-07 01:44:08 +0200

    Adds class methods to check degrees of separation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User
 
   has_many :both, :friends, type: :FRIENDSHIP, model_class: :User
 
+  def self.one_degree_of_separation?(current_user, target_user)
+    check_degrees_of_separation(1, current_user, target_user)
+  end
+
+  def self.two_degrees_of_separation?(current_user, target_user)
+    check_degrees_of_separation(2, current_user, target_user)
+  end
+
+  def self.check_degrees_of_separation(degrees = 1, current_user, target_user)
+    current_user.friends(rel_length: degrees + 1).where(id:target_user.id).any?
+  end
 end
