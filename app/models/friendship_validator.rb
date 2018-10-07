64c2b325f82b73df30e8ae3de818a579983fcbc6
develaper <lachguer.brahim@gmail.com>
2018-10-07 21:22:00 +0200

    Encapsulates the logic related to degrees of separation into a PORO

---
+++ app/models/friendship_validator.rb
@@ -0,0 +1,17 @@
+class FriendshipValidator
+
+  def initializer()
+  end
+
+  def one_degree_of_separation?(current_user, target_user)
+    check_degrees_of_separation(1, current_user, target_user)
+  end
+
+  def two_degrees_of_separation?(current_user, target_user)
+    check_degrees_of_separation(2, current_user, target_user)
+  end
+
+  def check_degrees_of_separation(degrees = 1, current_user, target_user)
+    current_user.friends(rel_length: degrees + 1).where(id:target_user.id).any?
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,6 @@ class User
 
   has_many :both, :friends, type: :FRIENDSHIP, model_class: :User
 
-  def self.one_degree_of_separation?(current_user, target_user)
-    check_degrees_of_separation(1, current_user, target_user)
-  end
-
-  def self.two_degrees_of_separation?(current_user, target_user)
-    check_degrees_of_separation(2, current_user, target_user)
-  end
-
-  def self.check_degrees_of_separation(degrees = 1, current_user, target_user)
-    current_user.friends(rel_length: degrees + 1).where(id:target_user.id).any?
-  end
-
   def geolocate_ip
     geolite_file = File.join(Rails.root, 'app', 'assets', 'GeoLite2', 'GeoLite2-City.mmdb')
     db = MaxMindDB.new(geolite_file)
