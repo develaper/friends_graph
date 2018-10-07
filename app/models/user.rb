6390a19e51414142e3d6db38629c71c3eff8a9cf
develaper <lachguer.brahim@gmail.com>
2018-10-07 20:57:49 +0200

    Adds geolocation by ip as a User instance method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User
   include Neo4j::ActiveNode
 
   property :name, type: String
+  property :ip, type: String
 
   has_many :both, :friends, type: :FRIENDSHIP, model_class: :User
 
@@ -15,5 +16,12 @@ class User
 
   def self.check_degrees_of_separation(degrees = 1, current_user, target_user)
     current_user.friends(rel_length: degrees + 1).where(id:target_user.id).any?
+  end
+
+  def geolocate_ip
+    geolite_file = File.join(Rails.root, 'app', 'assets', 'GeoLite2', 'GeoLite2-City.mmdb')
+    db = MaxMindDB.new(geolite_file)
+    ret = db.lookup( ip )
+    [ret.location.latitude , ret.location.longitude]
   end
 end
